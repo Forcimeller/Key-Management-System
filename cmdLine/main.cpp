170692c050023bf022e6c3374c84c1b484d49ed6
Jason Adoteye <jasonadoteye+dev@gmail.com>
2024-03-23 20:22:56 +0000

    Database can now be contacted by the program using lookup and insert. ticket complete.

--- cmdLine/main.cpp
+++ cmdLine/main.cpp
@@ -1,6 +1,43 @@
-#include <iostream>
+#include "main.h"
+
+void connectToDatabase();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    mongocxx::instance instance{};
+    mongocxx::client client{mongocxx::uri{}};
+
+    auto database = client["KeyManDatabase"];
+    auto collection = database["test"];
+
+    auto find_one_filtered_result = collection.find_one(make_document(kvp("i", 0)));
+
+    if (find_one_filtered_result) {
+        std::cout << "Yes" << std::endl;
+    } else {
+        std::cout << "No." << std::endl;
+    }
+
+    auto doc_value = make_document(
+            kvp("name", "MongoDB"),
+            kvp("type", "database"),
+            kvp("count", 1)
+    );
+
+    //auto doc_view = doc_value.view();
+
+    collection.insert_one(
+            static_cast<bsoncxx::v_noabi::document::view_or_value>(doc_value)
+            );
+
+    find_one_filtered_result = collection.find_one(make_document(kvp("name", "MongoDB")));
+
+    if (find_one_filtered_result) {
+        std::cout << "Yes" << std::endl;
+    } else {
+        std::cout << "No." << std::endl;
+    }
+
     return 0;
 }
+
---
+++ cmdLine/main.h
@@ -0,0 +1,24 @@
+//
+// Created by Jason Adoteye (M00659794)
+// for CST3990 on 13/02/2024
+//
+
+#ifndef CMDLINE_MAIN_H
+#define CMDLINE_MAIN_H
+
+#include <iostream>
+
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_array;
+using bsoncxx::builder::basic::make_document;
+
+#endif //CMDLINE_MAIN_H
