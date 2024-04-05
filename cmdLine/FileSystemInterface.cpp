1ae31201e82490352be98081c77397e6a6efd557
Jason Adoteye <jasonadoteye+dev@gmail.com>
2024-04-05 13:40:47 +0100

    Added File reader Class

---
+++ cmdLine/FileSystemInterface.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Jason Adoteye (M00659794)
+// for CST3990 on 29/03/24.
+//
+
+#include "FileSystemInterface.h"
---
+++ cmdLine/FileSystemInterface.h
@@ -0,0 +1,15 @@
+//
+// Created by Jason Adoteye (M00659794)
+// for CST3990 on 29/03/24.
+//
+
+#ifndef CMDLINE_FILESYSTEMINTERFACE_H
+#define CMDLINE_FILESYSTEMINTERFACE_H
+
+class FileSystemInterface {
+    
+
+};
+
+
+#endif //CMDLINE_FILESYSTEMINTERFACE_H
